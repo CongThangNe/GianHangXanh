5cfd8268c3cb1ed88f957f35f32649d87e32e355
lam4999 <lamdtph49959@gmail.com>
2025-11-01 21:14:19 +0700

    chuyển 1 số đường link sang admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,24 +54,6 @@
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Trang chủ</a></li>
           <li class="nav-item"><a class="nav-link" href="{{ route('cart.index') }}">Giỏ hàng</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('admin.products.index') }}">Quản lý sản phẩm</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('admin.categories.index') }}">Danh mục</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('admin.product_variants.index') }}">Quản lý biến thể</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('admin.discount-codes.index') }}">Mã Giảm Giá</a></li>
-          <!-- ✅ Dropdown Thuộc tính -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="attributeDropdown" role="button"
-               data-bs-toggle="dropdown" aria-expanded="false">
-              Thuộc tính
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="attributeDropdown">
-              <li><a class="dropdown-item" href="{{ route('admin.attributes.index') }}">Danh sách thuộc tính sản phẩm </a></li>
-              <li><a class="dropdown-item" href="{{ route('admin.attribute_values.index') }}">Giá trị thuộc tính</a></li>
-            </ul>
-
-          <li class="nav-item">
-            <a class="nav-link" href="/discount-events">Mã Giảm Giá</a>
-          </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ route('admin.dashboard') }}">Admin</a>
           </li>
