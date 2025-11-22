d89685e15b024a5deec70fdc5c3f634e57d85c93
vbvinh <vinhvuong357@gmail.com>
2025-11-22 14:01:42 +0700

    hoàn thiện checkout validate thông tin đơn hàng

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,23 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Order extends Model
 {
     use HasFactory;
-
+    
     protected $fillable = [
-        'user_id',
-        'discount_code_id',
-        'total_price',
-        'status'
+        'order_code', 'total', 'payment_method', 'status',
+        'customer_name', 'customer_phone', 'customer_address', 'note'
     ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function discountCode()
-    {
-        return $this->belongsTo(DiscountCode::class);
-    }
 
     public function details()
     {
