b8f4dbd994334692d64983f4e09a2e0ab1a818b4
lam4999 <lamdtph49959@gmail.com>
2025-12-11 01:03:34 +0700

    abc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -11,24 +12,41 @@ class HomeController extends Controller
     {
         $categories = Category::all();
         $keyword = $request->input('keyword');
+
+        // Thêm dòng FEATURED PRODUCTS
+        $featuredProducts = Product::latest()->take(4)->get();
+
         if ($keyword) {
             $products = Product::where('name', 'like', '%' . $keyword . '%')->paginate(12);
         } else {
             $products = Product::latest()->take(4)->get();
         }
-        return view('products.index', compact('categories','products', 'keyword'));
+
+        return view('products.index', compact(
+            'categories',
+            'products',
+            'keyword',
+            'featuredProducts'
+        ));
     }
 
     public function category($id)
     {
         $categories = Category::all();
         $products = Product::with('variants')->where('category_id', $id)->paginate(12);
-        return view('products.index', compact('categories','products'));
+        return view('products.index', compact('categories', 'products'));
     }
 
     public function show($id)
     {
         $product = Product::with('category', 'variants')->findOrFail($id);
         return view('products.show', compact('product'));
+    }
+    public function allProducts()
+    {
+        $categories = Category::all();
+        $products = Product::paginate(12);
+
+        return view('products.index', compact('categories', 'products'));
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::with('category')->paginate(20);
+        $products = Product::with('category')->paginate(10);
         return view('admin.products.index', compact('products'));
     }
 
