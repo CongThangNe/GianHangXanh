d65c33279932fe2dc7566f074197580f9e9b94cf
hien-2004 <hienqph37213@fpt.edu.vn>
2025-12-20 04:02:39 +0700

    thống kê top sản phẩm bán chạy

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -15,24 +15,6 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        // Nếu là AJAX request → chỉ trả về phần nội dung
-        // if ($request->ajax()) {
-        //     return view('admin.dashboard');
-        // }
-
-        // Nếu là truy cập trực tiếp → load trong layout admin
-        // return view('layouts.admin', [
-        //     'content' => view('admin.dashboard')->render()
-        // ]);
-
-        // GET list top 3 best seller products
-        // $topSellingProducts = Product::withCount(['orderItems as total_sold' => function ($query) {
-        //     $query->select(\DB::raw("SUM(quantity)"));
-        // }])->orderByDesc('total_sold')->take(3)->get();
-
-        // return view('admin.dashboard', compact('users', 'categories', 'products' , 'orders' , '$topSellingProducts'));
-
-        // Tổng quan
         $userCount  = User::count();
         $orderCount = Order::count();
         $revenue = Order::where('status', 'paid')->sum('total');
@@ -41,7 +23,7 @@ class DashboardController extends Controller
         // Danh sách user mới
         $users = User::latest()->take(5)->get();
 
-        // 🔥 TOP 3 SẢN PHẨM BÁN CHẠY
+        
        $topSellingProducts = Product::withSum(
             ['orderDetails as total_sold' => function ($query) {
                     $query->whereHas('order', function ($q) {
