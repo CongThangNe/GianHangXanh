1f657e4b8d866159c02ed299e4d4f57fd1a022b4
Thành Vinh <thanhvinh17032004@gmail.com>
2025-12-25 01:25:20 +0700

    tin tuc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,6 +113,9 @@
                             <a class="text-sm font-medium hover:text-primary" href="{{ route('support.index') }}">
                                 Liên hệ & Hỗ trợ
                             </a>
+                            <a class="text-sm font-medium hover:text-primary" href="{{ route('news.index') }}">
+                                Tin tức
+                            </a>
 
                             {{-- ❌ ĐÃ XOÁ ADMIN KHỎI MENU NGANG --}}
                         </nav>
